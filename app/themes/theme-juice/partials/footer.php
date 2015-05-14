9ae3452b48bfb6834411ebffd121ef820d01f602
ezekg <ezekg@yahoo.com>
2015-05-14 16:07:21 -0500

    remove theme files from app/

--- app/themes/theme-juice/partials/footer.php
+++
@@ -1,9 +0,0 @@
-<div class="footer__wrapper">
-  <div class="footer">
-    <div class="footer__navigation">
-      <?php wp_nav_menu( array( "theme_location" => "footer_nav" ) ); ?>
-    </div>
-  </div>
-</div>
-<!-- / footer -->
-<?php $theme->render_footer(); ?>
