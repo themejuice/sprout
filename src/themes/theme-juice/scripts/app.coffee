c89611563bf89bd6122c9d4001c636cda17f6bff
ezekg <ezekg@yahoo.com>
2015-06-05 20:27:00 -0500

    remove trace of browserify

--- src/themes/theme-juice/scripts/app.coffee
+++ src/themes/theme-juice/scripts/app.coffee
@@ -1,5 +1,3 @@
 "use-strict"
 
-require "jquery"
-
 $(document).ready ->
--- src/themes/theme-juice/scripts/app.js
+++ src/themes/theme-juice/scripts/app.js
@@ -1,7 +1,5 @@
 (function() {
   "use-strict";
-  require("jquery");
-
   $(document).ready(function() {});
 
 }).call(this);
